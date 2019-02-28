bc6723b1e27b948eaee45e148cd9e32b1f2a4a0a
TheWussPuss <kapsmudit@gmail.com>
2019-02-28 20:32:51 +0530

    as

---
+++ index.js
@@ -0,0 +1,63 @@
+const request = require('request');
+const path = require('path');
+const events = require('events');
+// const http = require('http');
+// const fs = require('fs');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+let key = '5E563EF97775105125A5E5C3E3AE39FE';
+
+let steam_id = '';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+function getPlayerDetails(player_id, app_res) {
+	let url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${player_id}`;
+	request(url, (err, res, body) => {
+		if (err) throw err;
+		body = JSON.parse(body);
+		player = body.response.players[0];
+		console.log(player);
+		let personastate = ['offline', 'online', 'busy', 'away', 'snooze', 'looking to trade', 'looking to play'][player.personastate];
+		app_res.send(`<p>${player.personaname}</p>` + 
+			`<img src=${player.avatarfull}/>` + 
+			`<p> Last online: ${Date(player.lastlogoff)}</p>` + 
+			`<p> Currently: ${personastate} </p>`);
+	})
+
+}
+
+
+
+app.post('/', (req, app_res) => {
+    // console.log(req);
+    player_id = req.body.player_id;
+    if(!/^\d{17}$/.test(player_id)) {
+        let vanity_user_url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${key}&vanityurl=${player_id}`;
+        request(vanity_user_url, (err, res, body) => {
+            if (err) throw err;
+            body = JSON.parse(body);
+            if (body.response.success == 42) {
+                app_res.send('Invalid username/id');
+            }
+            else {
+				player_id = body.response.steamid;
+				getPlayerDetails(player_id, app_res);
+            }
+            
+		})
+	}
+	else {
+		getPlayerDetails(player_id, app_res);
+	}	
+})
+
+app.listen(5000);
